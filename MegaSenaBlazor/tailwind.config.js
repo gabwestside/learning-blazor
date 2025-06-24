3d3ccdccfb7a9856ec1cee45d4ad500ad45758c7
Gabriel Moura <gabriel.lima@aspec.com.br>
2025-06-24 13:55:46 -0300

    feat: install tailwindcss

---
+++ MegaSenaBlazor/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+    content: [
+        "./**/*.{html,js,razor}"
+    ],
+    theme: {
+        colors: {
+            "primary": "#8625D2",
+            "white": "#FFFFFF"
+        },
+        fontFamily: {
+        },
+        extend: {},
+    },
+    plugins: [],
+}
